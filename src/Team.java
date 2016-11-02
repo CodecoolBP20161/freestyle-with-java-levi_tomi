1e4cae12ab3d969eec796983c221481c608a1b14
matheisson <csanyil@gmail.com>
2016-11-02 13:26:08 +0100

    team implemented

---
+++ src/Team.java
@@ -0,0 +1,20 @@
+/**
+ * Created by levente on 2016.11.02..
+ */
+public class Team {
+
+
+        String name;
+        int attack;
+        int defence;
+        int points;
+
+        public Team(String name, int attack, int defence, int points) {
+            this.name = name;
+            this.attack = attack;
+            this.defence = defence;
+            this.points = points;
+        }
+
+
+}
