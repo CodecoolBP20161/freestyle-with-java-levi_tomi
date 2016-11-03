0eac6b3abff10e5016cc9a557e7c500ac6162c12
matheisson <csanyil@gmail.com>
2016-11-03 11:52:01 +0100

    match engine 99%. Standing needs to be implemented.

--- src/Match.java
+++ src/Match.java
@@ -4,7 +4,6 @@
 
 public class Match {
 
-
     static int homeboost = (int)(Math.random() * 8 + 1);
 
     public static double chanceToWin(Team team){
@@ -13,7 +12,6 @@ public class Match {
 
     public static void boostHome(Team team) {
         double luck = (double) Math.random() + 1;
-        System.out.println("l --"+luck);
         double homeAdvantage = Match.homeboost;
         team.chance += homeAdvantage;
         team.chance *= luck;
@@ -21,31 +19,44 @@ public class Match {
 
     public static void luckAway(Team team) {
         double luck = (double)Math.random() +1 ;
-        System.out.println("l2 --"+luck);
         team.chance *= luck;
     }
 
     public static String theMatch(Team t1, Team t2) {
-        Match.chanceToWin(t1);
-        Match.chanceToWin(t2);
-        Match.boostHome(t1);
-        Match.luckAway(t2);
-        // MATH ABS????!
-        if (Math.abs(t1.chance - t2.chance) > 15) {
-            if (t1.chance > t2.chance){
-                return t1.name + " has won the game.";
+        int turns = 0;
+        while (turns != 5) {
+            Match.chanceToWin(t1);
+            Match.chanceToWin(t2);
+            Match.boostHome(t1);
+            Match.luckAway(t2);
+//            System.out.println("home " +t1.chance);
+//            System.out.println("b  " +t2.chance);
+            if (Math.abs(t1.chance - t2.chance) > 20) {
+                if (t1.chance > t2.chance) {
+                    turns++;
+                    t1.goal += 1;
+                    System.out.println(t1.name + " has scored.");
+                } else {
+                    turns++;
+                    t2.goal += 1;
+                    System.out.println(t2.name + " has scored.");
+                }
+            }else {
+                turns++;
+                System.out.println(t1.name + " and " + t2.name + " missed");
             }
-            return t2.name + " caused an upset, and won the game away!";
         }
-        return t1.name + " and " + t2.name + " played a draw...";
-    }
+        System.out.println("Score = "+ t1.goal + " "+ t2.goal);
+//        return "Score = "+ t1.goal + " "+ t2.goal;
+        return "done";
+        }
 
 
 
     public static void main(String[] args) {
 
-        Team bse = new Team("A", 50, 50);
-        Team ifi = new Team("B", 50, 50);
+        Team bse = new Team("Ferencváros", 50, 50);
+        Team ifi = new Team("Újpest", 50, 50);
 
         Match elso = new Match();
         String x = elso.theMatch(bse,ifi);
@@ -53,21 +64,6 @@ public class Match {
         System.out.println("b  " +ifi.chance);
 
         System.out.println(x);
-//        System.out.println(bse.goal);
-
-
 
     }
 }
-
-
-// may'll be useful later... Line 34
-//if (Math.abs(t1.chance - t2.chance) > 15) {
-//        t1.goal += 1;
-//        return t1.name +" won the match";
-//
-//        } else if (t1.chance < t2.chance){
-//        t2.goal += 1;
-//        return t2.name +" won the match";
-//        }
-//        return "Draw ";
